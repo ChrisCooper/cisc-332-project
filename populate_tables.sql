fd193a283064cbaf1590a712d1167d363a7591e1
Chris Cooper <chriscooper1991@gmail.com>
2013-03-05 15:40:50 -0500

    Added test data

--- populate_tables.sql
+++ populate_tables.sql
@@ -11,12 +11,12 @@ insert into Location values
 ;
 
 insert into Car values
-(0, 'U92N3ID9IJ2EKJD9IJ', 'Mazda', '6', 2012, 3817.4, 11.2, 0),
-(1, 'JD982JOIS98J239873', 'Subaru', 'Forester', 2013, 872.9, 10.0, 0),
-(2, '1M8GDM9AXKP0427982', 'Honda', 'Civic', 2012, 3817.4, 11.2, 1),
-(3, '8DJN2O28XJ9W8J3KND', 'Ford', 'Focus', 2011, 9873.2, 21.0, 1),
-(4, 'ASIX3OI2MDKMO98392', 'Lamborghini', 'Murcielago', 2013, 9274.0, 19.5, 2),
-(5, 'OIWJD893429849J245', 'Ferrari', 'Enzo', 2012, 4652.1, 4.6, 2)
+(0, 'U92N3ID9IJ2EKJD9IJ', 'Mazda', '6', 2012, 38317.4, 11.2, 0),
+(1, 'JD982JOIS98J239873', 'Subaru', 'Forester', 20513, 872.9, 10.0, 0),
+(2, '1M8GDM9AXKP0427982', 'Honda', 'Civic', 2012, 38317.4, 11.2, 1),
+(3, '8DJN2O28XJ9W8J3KND', 'Ford', 'Focus', 2011, 96873.2, 21.0, 1),
+(4, 'ASIX3OI2MDKMO98392', 'Lamborghini', 'Murcielago', 2013, 92774.0, 19.5, 2),
+(5, 'OIWJD893429849J245', 'Ferrari', 'Enzo', 2012, 46352.1, 4.6, 2)
 ;
 
 insert into Fee values
@@ -25,71 +25,48 @@ insert into Fee values
 ;
 
 insert into Reservation values
-(0, '2013-Mar-04', '13:00', 5, 0, 0),
+(0, '2013-03-03', '17:30', 3, 3, 1, 2),
+(1, '2013-03-07', '10:30', 5, 0, 0, 0),
+(2, '2013-02-27', '15:00', 4, 4, 0, 2),
+(3, '2013-02-23', '14:00', 5, 5, 1, 2),
+(4, '2013-02-27', '09:00', 5, 5, 2, 2),
+(5, '2013-03-13', '09:30', 2, 2, 0, 0),
+(6, '2013-03-07', '09:00', 4, 4, 2, 2),
+(7, '2013-02-25', '11:00', 6, 2, 2, 0),
+(8, '2013-03-02', '10:00', 3, 0, 0, 2),
+(9, '2013-03-13', '12:30', 4, 1, 2, 2),
+(10, '2013-03-10', '11:30', 6, 4, 2, 1),
+(11, '2013-03-09', '10:00', 1, 3, 0, 0)
 ;
 
--- CREATE TABLE Reservation (
---     --Simple attributes
---     ResNum          INTEGER     NOT NULL,
---     PickupDate             Date NOT NULL,
---     PickupTime             Time NOT NULL,
---     Duration        INTEGER NOT NULL,
 
---     --Foreign keys
---     MemNum    INTEGER NOT NULL,
---     FOREIGN KEY(MemNum) REFERENCES Member,
---     VIN    INTEGER NOT NULL,
---     FOREIGN KEY(VIN) REFERENCES Car,
---     LocNum    INTEGER NOT NULL,
---     FOREIGN KEY(LocNum) REFERENCES Location,
+insert into Payment values
+(0, 12.50, '2013-03-03', 0),
+(1, 15.25, '2013-02-25', 1),
+(2, 84.50, '2013-02-26', 2)
+;
 
---     PRIMARY KEY(ResNum)
--- );
+insert into Maintenance values
+(0, '2013-01-03', 8572, 0),
+(1, '2013-01-25', 9805.6, 1),
+(2, '2012-12-13', 10641, 2),
+(3, '2013-01-26', 10982, 2)
+;
 
-
--- CREATE TABLE Payment (
---     --Simple attributes
---     PaymentNum          INTEGER     NOT NULL,
---     Amount              money NOT NULL,
---     Paydate           DATE NOT NULL,
-
---     --Foreign keys
---     MemNum    INTEGER NOT NULL,
---     FOREIGN KEY(MemNum) REFERENCES Member,
-
---     PRIMARY KEY(PaymentNum)
--- );
-
-
-
--- CREATE TABLE Maintenance (
---     --Simple attributes
---     MaintNum          INTEGER     NOT NULL,
---     CheckupDate             VARCHAR(20) NOT NULL,
---     Odometer    DOUBLE PRECISION NOT NULL,
-
---     --Foreign keys
---     VIN    INTEGER NOT NULL,
---     FOREIGN KEY(VIN) REFERENCES Car,
-
---     PRIMARY KEY(MaintNum)
--- );
-
-
-
--- CREATE TABLE Charge (
---     --Simple attributes
---     ChargeNum          INTEGER     NOT NULL,
---     Paydate           DATE NOT NULL,
-
---     --Foreign keys
---     FeeNum    INTEGER NOT NULL,
---     ResNum    INTEGER NOT NULL,
---     MemNum    INTEGER NOT NULL,
-
---     FOREIGN KEY(FeeNum) REFERENCES Fee,
---     FOREIGN KEY(ResNum) REFERENCES Reservation,
---     FOREIGN KEY(MemNum) REFERENCES Member,
-
---     PRIMARY KEY(ChargeNum)
--- );
+insert into Charge values
+(0, '2013-03-03', 0, 0, 1),
+(1, '2013-03-07', 0, 1, 0),
+(2, '2013-02-27', 0, 2, 0),
+(3, '2013-02-23', 0, 3, 1),
+(4, '2013-02-27', 0, 4, 2),
+(5, '2013-03-13', 0, 5, 0),
+(6, '2013-03-07', 0, 6, 2),
+(7, '2013-02-25', 0, 7, 2),
+(8, '2013-03-02', 0, 8, 0),
+(9, '2013-03-13', 0, 9, 2),
+(10, '2013-03-10', 0, 10, 2),
+(11, '2013-03-09', 0, 11, 0),
+(12, '2013-03-13', 1, null, 0),
+(13, '2013-03-10', 1, null, 1),
+(14, '2013-03-09', 1, null, 2)
+;
